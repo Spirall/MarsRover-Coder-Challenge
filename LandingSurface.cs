dc7cb642d5035504a32a09923d96f22cfc70e6fc
Thabo Buthelezi <37297137+Spirall@users.noreply.github.com>
2019-02-20 21:30:09 +0200

    Navigation Location

---
+++ LandingSurface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Next24_Mars_Rover_Code_Challenge.NavCommandCentre;
+
+namespace Next24_Mars_Rover_Code_Challenge.NavigationLocation
+{
+    public interface LandingSurface : Commands
+    {
+        void SetSize(ZoneSize aSize);
+        ZoneSize GetSize();
+        bool IsValid(MovementPoints aPoints);
+    }
+}
---
+++ MovementPoints.cs
@@ -0,0 +1,20 @@
+﻿
+/*
+● The zones have been very carefully surveyed ahead of time and are deemed safe for
+exploration within the landing terrain bounds, as represented by a single cartesian
+coordinate - for example: (5, 5).*/
+
+namespace Next24_Mars_Rover_Code_Challenge.NavigationLocation
+{
+    public struct MovementPoints
+    {
+        public int X;
+        public int Y;
+
+        public MovementPoints(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
