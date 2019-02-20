748f3810db9e4374bbdb9615f27a94f12b201781
Thabo Buthelezi <37297137+Spirall@users.noreply.github.com>
2019-02-20 21:31:55 +0200

    Command Center

---
+++ CommandTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Next24_Mars_Rover_Code_Challenge.NavCommandCentre
+{
+    public enum CommandTypes
+    {
+        LandingSurfaceSizeCommand,
+        RoverDeployCommand,
+        RoverExploreCommand
+    }
+}
---
+++ LandingSurfaceSizeCommand.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Drawing;
+
+
+namespace Next24_Mars_Rover_Code_Challenge.NavCommandCentre
+{
+    public interface LandingSurfaceSizeCommand : Commands
+    {
+        Size size { get; }
+        void SetReceiver(LandingSurfaceSizeCommand aLandingSurface);
+    }
+}
